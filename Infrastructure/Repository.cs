4ab906d9016e3da42badcf7677b295cb51f4746d
Lukas <lukas@youtuberis.lt>
2019-09-15 14:26:55 +0200

    [wip] Implement static repository

---
+++ Infrastructure/Repository.cs
@@ -0,0 +1,22 @@
+﻿using MandotaryAssignment01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MandotaryAssignment01.Infrastructure
+{
+    public static class Repository
+    {
+        public static List<Product> Products = new List<Product>();
+        public static List<Invoice> Invoices = new List<Invoice>();
+
+        static Repository() {
+            Book book1 = new Book("Georg Martin", "With a Little Help from My Friends: The  Making of Sgt. Pepper", 1995);
+
+            this.Products.Add(book1);
+
+
+        }
+    }
+}
