aa2dd4908ed72be38f18410f7c1361e983f87ab2
Lukas <lukas@youtuberis.lt>
2019-10-17 09:12:12 +0200

    Add AddToCart button component

---
+++ ViewComponents/AddToCart.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using MandotaryAssignment01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MandotaryAssignment01.ViewComponents
+{
+    public class AddToCart : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+            return View(productId);
+        }
+    }
+}
