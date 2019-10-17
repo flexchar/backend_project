8531fc5128bf3c8c8522498931ffbee507bfd47f
Lukas <lukas@youtuberis.lt>
2019-10-17 09:07:19 +0200

    Add JS function for fetching random images

--- Assets/app.js
+++ Assets/app.js
@@ -1,1 +1,21 @@
 import './app.scss';
+
+const randomUrl = iterator =>
+    `https://source.unsplash.com/random/500x300/?${iterator}`;
+
+function loadRandomImages() {
+    const images = document.querySelectorAll('img[data-random]');
+
+    let iterator = 0;
+    images.forEach(img => img.setAttribute('src', randomUrl(++iterator)));
+}
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        loadRandomImages();
+    },
+    {
+        passive: true,
+    }
+);
