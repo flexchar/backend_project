4a01cf1bcfbaf32b1784d26897c475b108091320
Lukas <lukas@youtuberis.lt>
2019-10-26 17:27:04 +0200

    Add navigation categories component

---
+++ ViewComponents/NavigationCategories.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MandotaryAssignment01.ViewComponents
+{
+    public class NavigationCategories : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            IEnumerable categories = new List<string> {
+                 "Book", "MusicCD", "Movie"
+            };
+
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            return View(categories);
+        }
+    }
+}
